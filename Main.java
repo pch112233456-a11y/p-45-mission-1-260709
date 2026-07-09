3bd5276964972444db96b91ced3bb240e2fcc827
ChangHyeon Park <pch112233456@gmail.com>
2026-07-09 15:04:01 +0900

    PR 날리기

---
+++ Main.java
@@ -0,0 +1,10 @@
+package org.example.day1.name;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("이름 : 박창현");
+
+        System.out.println("나이 : 27");
+    }
+}
